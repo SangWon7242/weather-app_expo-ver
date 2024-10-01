5f3b28b03bd520ff638e3b212e6a969c63c00493
SangWon7242 <gotkdqja@gmail.com>
2024-10-01 11:16:49 +0900

    날씨 앱 스타일 적용

--- App.js
+++ App.js
@@ -4,8 +4,18 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Ansan</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.regDate}>Friday, 20, January</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+        <View style={styles.tempCon}>
+          <Text style={styles.temp}>31</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +23,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: "#ffff42",
+  },
+  city: {
+    flex: 1,
     justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    color: "black",
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  regDate: {
+    borderRadius: 20,
+    backgroundColor: "black",
+    color: "#ffff42",
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  desc: {
+    marginTop: 10,
+    color: "black",
+    fontSize: 17,
+  },
+  tempCon: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  temp: {
+    color: "black",
+    fontSize: 173,
   },
 });
