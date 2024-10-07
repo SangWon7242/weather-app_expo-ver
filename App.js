4e027cc698cdd3a37c4a5ea7a6af1046b7e9e05a
SangWon7242 <gotkdqja@gmail.com>
2024-10-07 18:02:24 +0900

    위치 가져오고 적용하기

--- App.js
+++ App.js
@@ -1,13 +1,55 @@
+import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
+
+import * as Location from "expo-location";
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
 
 export default function App() {
+  const [city, setCity] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  const [ok, setOk] = useState(true);
+
+  const locationData = async () => {
+    /*
+    const permission = await Location.requestForegroundPermissionsAsync();
+    console.log(permission);
+    */
+
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false); // 권한 부여 실패, 해당 값에 따라 다른 UI창을 보여줄 수 있음
+      setErrorMsg("위치에 대한 권한 부여가 거부되었습니다.");
+
+      return;
+    }
+
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+
+    const address = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    console.log(address);
+    console.log(address[0].city);
+
+    const cityAdress = address[0].city;
+    setCity(cityAdress);
+  };
+
+  useEffect(() => {
+    locationData();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Ansan</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         horizontal
@@ -21,7 +63,7 @@ export default function App() {
             <Text style={styles.desc}>Sunny</Text>
           </View>
           <View style={styles.tempCon}>
-            <Text style={styles.temp}>31</Text>
+            <Text style={styles.temp}>29</Text>
           </View>
         </View>
         <View style={styles.weatherInner}>
